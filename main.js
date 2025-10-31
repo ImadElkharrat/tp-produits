9b06516a45421b83d2a09ecacfaaa571c773f456
ImadElkharrat <imad.elkharrat@gmail.com>
2025-10-31 17:27:29 +0100

    FCommit

--- main.js
+++ main.js
@@ -1,36 +1,60 @@
-import { PRODUCTS } from "./data/products.js"; 
-import { isExpired } from "./utils/date.js"; 
+import { PRODUCTS } from "./data/products.js";
+import { isExpired } from "./utils/date.js";
+
+
+console.log(`${PRODUCTS.length} produits importés.\n`);
+
 
 PRODUCTS.forEach((p) => console.log(p.name)); 
+console.log("\n");
 
 
-const expired = PRODUCTS.filter(p => isExpired(p.expiryDate)); 
+
+
+const expired = PRODUCTS.filter(p => isExpired(p.expiryDate));
 console.log("Produits expirés :", expired);
+console.log("\n");
 
-const total = PRODUCTS.reduce( 
-(somme, p) => somme + p.price * p.quantity, 
-0 
-); 
-console.log("Valeur totale du stock :", total, "MAD"); 
 
-const promo = PRODUCTS.filter(p => p.tags.includes("promo")); 
-console.log("Produits en promo :", promo.map(p => p.name)); 
-const sorted = [...PRODUCTS].sort((a, b) => a.price - b.price); 
-console.log("Tri par prix croissant :", sorted.map(p => 
-p.name));
+const total = PRODUCTS.reduce(
+  (somme, p) => somme + p.price * p.quantity,
+  0
+);
+console.log("Valeur totale du stock :", total, "MAD");
+console.log("\n");
 
-const delay = (ms) => new Promise(res => setTimeout(res, ms)); 
-const addProduct = async (list, newP) => { 
-await delay(300); 
-const id = Math.max(...list.map(p => p.id)) + 1; 
-return [...list, { id, ...newP }]; 
-}; 
-const newList = await addProduct(PRODUCTS, { 
-name: "Savon", 
-category: "Hygiène", 
-price: 5, 
-quantity: 10, 
-expiryDate: "2026-01-01", 
-tags: ["hygiene"] 
-}); 
+
+const promo = PRODUCTS.filter(p => p.tags.includes("promo"));
+console.log("Produits en promo :", promo.map(p => p.name));
+
+
+const sorted = [...PRODUCTS].sort((a, b) => a.price - b.price);
+console.log("Tri par prix croissant :", sorted.map(p => p.name));
+console.log("\n");
+
+
+
+const delay = (ms) => new Promise(res => setTimeout(res, ms));
+
+
+const addProduct = async (list, newP) => {
+  await delay(300); 
+  
+  const id = Math.max(...list.map(p => p.id)) + 1;
+  
+  return [...list, { id, ...newP }];
+};
+
+
+const newList = await addProduct(PRODUCTS, {
+  name: "Savon",
+  category: "Hygiène",
+  price: 5,
+  currency: "MAD", 
+  quantity: 10,
+  expiryDate: "2026-01-01",
+  tags: ["hygiene"]
+});
+
 console.log("Après ajout :", newList.length, "produits");
+
